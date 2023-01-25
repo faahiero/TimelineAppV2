e690995669cae076b0d06dd8b881014f31f166c8
Fabricio Silva <fabriciosilvalp@outlook.com>
2023-01-25 16:55:07 -0300

    develop branch

--- modules/generate_visualization.py
+++ modules/generate_visualization.py
@@ -2,6 +2,10 @@ import os
 import shutil
 import time
 import webbrowser
+
+import dash
+from dash import Input, Output, dcc, html
+import plotly.graph_objects as go
 import pandas as pd
 import folium
 
@@ -10,7 +14,16 @@ import folium
 def generate_visualization():
     timestamp_fname = time.strftime("%Y%m%d-%H%M%S") + "_"
     df = pd.read_csv("person_info.csv")
-    df.to_csv("person_info.csv", index=False)
+
+    df["Século"] = ""
+
+    for i in range(len(df)):
+        year = int(df.loc[i, "Data de Nascimento"].split()[-1])
+        if year % 100 == 0:
+            year -= 1
+        century = (year // 100) + 1
+        df.loc[i, "Século"] = century
+
     folium_map = folium.Map()
 
     for idx, linha in df.iterrows():
@@ -48,5 +61,103 @@ def generate_visualization():
 
     shutil.move('person_info.csv', 'data/' +
                 timestamp_fname + 'person_info.csv')
-    webbrowser.open('file://' + os.path.realpath('data/' +
-                    timestamp_fname + 'map.html'))
+
+    app = dash.Dash(__name__)
+
+    data = [go.Scattermapbox(
+        lat=df['Latitude'],
+        lon=df['Longitude'],
+        mode='markers',
+        marker=go.scattermapbox.Marker(
+            size=17,
+            color=df['Século'],
+            colorscale='Viridis',
+            showscale=True
+        ),
+    )]
+
+    layout = go.Layout(
+        autosize=True,
+        hovermode='closest',
+        mapbox=go.layout.Mapbox(
+            accesstoken='pk.eyJ1IjoiZmFhaGllcm8iLCJhIjoiY2txc3lnazRiMXNvNTJvcjFvMnp0aGd4NyJ9.fPzGgM_mC-xJnx56RQ_y8Q',
+            zoom=2
+        ),
+        height=800,
+        margin={"r": 0, "t": 60, "l": 0, "b": 0},
+    )
+
+    app.layout = dash.html.Div([
+
+        # iframe
+        dash.html.Iframe(
+            id='map',
+            srcDoc=open('data/' + timestamp_fname + 'map.html', 'r').read(),
+            width='100%',
+            height='100%'
+        ),
+
+        dcc.Graph(id='dash-map', figure={'data': data, 'layout': layout}),
+
+        dash.html.Div(dcc.RangeSlider(
+            id='seculo-slider',
+            min=df['Século'].min(),
+            max=df['Século'].max(),
+            value=[df['Século'].min(), df['Século'].max()],
+            marks={str(seculo): str(seculo) for seculo in df['Século'].unique()},
+            step=None
+        ), style=dict(
+            width='100%',
+            verticalAlign="middle",
+            padding="10px 0px 0px 0px",
+            display='inline-block',
+            textAlign='center',
+            fontSize=20,
+            fontFamily='Fira Sans',
+            fontWeight='bold',
+            color='black',
+            backgroundColor='white',
+            border='1px solid black',
+            borderRadius='5px',
+            margin='10px 0px 0px 0px',
+        )),
+    ])
+
+    @app.callback(
+        Output('dash-map', 'figure'),
+        [Input('seculo-slider', 'value')])
+    def update_figure(selected_seculo):
+        filtered_df = df[df['Século'].between(selected_seculo[0], selected_seculo[1])]
+
+        traces = []
+        traces.append(go.Scattermapbox(
+            lat=filtered_df['Latitude'],
+            lon=filtered_df['Longitude'],
+            mode='markers',
+            marker=go.scattermapbox.Marker(
+                size=17,
+                symbol='circle',
+            ),
+
+            hoverlabel=dict(
+                bgcolor="blue",
+                font_size=16,
+                font_family="Fira Sans"
+            ),
+            text="<b>Nome: </b>" + filtered_df['Nome Completo'] + "<br>" +
+                 "<b>Data de Nascimento: </b>" + filtered_df['Data de Nascimento'] + "<br>" +
+                 "<b>Local de Nascimento: </b>" + filtered_df['Local de Nascimento'] + "<br>" +
+                 "<b>Local de Falecimento: </b>" + filtered_df['Local de Falecimento'] + "<br>" +
+                 "<b>Latitude: </b>" + filtered_df['Latitude'].astype(str) + "<br>" +
+                 "<b>Longitude: </b>" + filtered_df['Longitude'].astype(str) + "<br>" +
+                 "<b>Século: </b>" + filtered_df['Século'].astype(str),
+        ))
+        return {
+            'data': traces,
+            'layout': layout
+        }
+
+    app.run_server()
+
+    # webbrowser.open('file://' + os.path.realpath('data/' +
+    #                 timestamp_fname + 'map.html'))
